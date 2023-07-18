7ddcc7c0ba77b2ebcffb671aacc7310cc67dc2cd
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-07-18 21:49:50 +0200

    feat: init and deinit with @AppStorage and UserDefaults

--- WeakSelfBootcamp/AppEntry.swift
+++ WeakSelfBootcamp/AppEntry.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WeakSelfBootcampApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            WeakSelfBootcamp()
         }
     }
 }
---
+++ WeakSelfBootcamp/WeakSelfBootcamp.swift
@@ -0,0 +1,79 @@
+//
+//  WeakSelfBootcamp.swift
+//  WeakSelfBootcamp
+//
+//  Created by Marcin Jędrzejak on 17/07/2023.
+//
+
+import SwiftUI
+
+struct WeakSelfBootcamp: View {
+    
+    @AppStorage("count") var count: Int?
+    
+    init() {
+        count = 0
+    }
+    
+    var body: some View {
+        NavigationView {
+            NavigationLink("Navigate", destination: WeakSelfSecondScreen())
+                .navigationTitle("Screen 1")
+        }
+        .overlay(
+            Text("\(count ?? 0)")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.green.cornerRadius(10))
+            , alignment: .topTrailing
+        )
+    }
+}
+
+struct WeakSelfSecondScreen: View {
+    
+    @StateObject var vm = WeakSelfSecondScreenViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("Second View")
+                .font(.largeTitle)
+                .foregroundColor(.red)
+            
+            if let data = vm.data {
+                Text(data)
+            }
+        }
+    }
+}
+
+class WeakSelfSecondScreenViewModel: ObservableObject {
+    
+    @Published var data: String? = nil
+    
+    init() {
+        print("INITIALIZE NOW")
+        let currentCount = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(currentCount + 1, forKey: "count")
+        getData()
+    }
+    
+    deinit {
+        print("DEINITIALIZE NOW")
+        let currentCount = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(currentCount - 1, forKey: "count")
+    }
+    
+    func getData() {
+        
+        DispatchQueue.global().async {
+            self.data = "NEW DATA!!!!"
+        }
+    }
+}
+
+struct WeakSelfBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        WeakSelfBootcamp()
+    }
+}
